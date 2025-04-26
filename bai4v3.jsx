ec7cb57e83c4c7d95f95cf65bc65cc0b63951766
Dung20012 <hantho1107@gmail.com>
2025-04-26 13:10:31 +0700

    Create bai4v3.jsx

---
+++ bai4v3.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./home.css"
+
+export default function Home() {
+  return (
+    <div>
+       <nav>
+            <a href="">Home </a>
+            <a href="">Contact </a>
+            <a href="">Home</a>
+       </nav>
+    </div>
+  )
+}
