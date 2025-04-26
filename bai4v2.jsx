388a8338d014cbc8ac2e4d92713075f8b78bef1a
Dung20012 <hantho1107@gmail.com>
2025-04-26 13:10:16 +0700

    Create bai4v2.jsx

---
+++ bai4v2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+        <nav>
+            <a href="">Home </a>
+            <a href="">Contact </a>
+            <a href="">Home</a>
+       </nav>
+    </div>
+  )
+}
