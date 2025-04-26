0e11f986e3c47a86964ebcfd6c7887dc58a81f04
Dung20012 <hantho1107@gmail.com>
2025-04-26 13:21:02 +0700

    Update bai5.jsx

--- bai5.jsx
+++ bai5.jsx
@@ -1,65 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Contact from './pages/contact/Contact'
+import Login from './pages/login/Login'
+import Product from './pages/product/Product'
+import Detail from './pages/detail/Detail'
 
-
-const Home = () => <h2>Đây là trang Home</h2>;
-const Product = () => <h2>Đây là trang Product</h2>;
-const Detail = () => <h2>Đây là trang Detail</h2>;
-
-
-const Header = () => {
-  const activeStyle = {
-    fontWeight: "bold",
-    color: "blue",
-    textDecoration: "underline",
-  };
-
+function App() {
   return (
-    <nav style={{ marginBottom: "20px" }}>
-      <NavLink
-        to="/"
-        style={({ isActive }) => (isActive ? activeStyle : undefined)}
-      >
-        Home
-      </NavLink>{" "}
-      |{" "}
-      <NavLink
-        to="/product"
-        style={({ isActive }) => (isActive ? activeStyle : undefined)}
-      >
-        Product
-      </NavLink>{" "}
-      |{" "}
-      <NavLink
-        to="/detail"
-        style={({ isActive }) => (isActive ? activeStyle : undefined)}
-      >
-        Detail
-      </NavLink>
-    </nav>
-  );
-};
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} /> // 5173 sẽ ra home(đỏ) product detail
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/product' element={<Product />} /> // xuộc vào product sẽ ra home product(đỏ) detail
+        <Route path='/detail' element={<Detail />} /> // xuộc vào detail sẽ ra home product detail(đỏ)
 
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div style={{ padding: "20px", fontFamily: "Arial" }}>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/detail" element={<Detail />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-};
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default App
